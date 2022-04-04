d04d0bb9e74c6b005c1cce32e07b7e1c723a4354
Vo Dinh Phuc <phucatoniegriezman@gmail.com>
2022-04-05 01:36:18 +0700

    Completed GUI of ORDER MANAGEMENT
    - Screen for list of orders
    - Screen for adding new order
    - Screed for updating and viewing detail order.

--- MyShop/ViewModel/MainViewModel.cs
+++ MyShop/ViewModel/MainViewModel.cs
@@ -9,10 +9,11 @@ namespace MyShop.ViewModel
     public class MainViewModel: BaseViewModel
     {
         public BaseViewModel CurrentViewModel { get; set; }
-
+        public BaseViewModel OrderManagementViewModel { get; set; }
         public MainViewModel()
         {
             CurrentViewModel = new DashboardViewModel();
+            OrderManagementViewModel = new OrderManagementViewModel();
         }
     }
 }
