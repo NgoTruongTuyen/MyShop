eed6e1c72c3c3252c3aa0e508cffdda16a86bc75
dankhoi24 <YeDanKhoi@gmail.com>
2022-04-04 20:48:59 +0700

    Create DashBoardView + ViewModel, MainViewModel
    DashBoardView:
             Create basic UI
    DashBoardViewModel:
             Connect to DashBoardView
    MainViewModel:
             Assign CurrentViewModel = DashBoardViewModel (fixed)
    MainWindow.xaml:
            Binding DashBoard tabItem to DashBoardView
    App.xaml
            Delete StartUri and replay it in App.xaml.cs
    App.xaml.cs:
            Generate MainWindow with DataContent equal to MainViewModel

--- MyShop/View/DashboardView.xaml.cs
+++ MyShop/View/DashboardView.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MyShop
+namespace MyShop.View
 {
     /// <summary>
-    /// Interaction logic for DashboardWindow.xaml
+    /// Interaction logic for DashboardView.xaml
     /// </summary>
-    public partial class DashboardWindow : Window
+    public partial class DashboardView : UserControl
     {
-        public DashboardWindow()
+        public DashboardView()
         {
             InitializeComponent();
         }
