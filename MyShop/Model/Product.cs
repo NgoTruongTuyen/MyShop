c7db19f15c806c15d1debb5dc8168b1d923cb989
NgoTruongTuyen <ngotuyen781@gmail.com>
2022-04-04 12:13:25 +0700

    Initial commit

---
+++ MyShop/Model/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Model
+{
+    public class Product : INotifyPropertyChanged
+    {
+        public String ProductId { get; set; }
+        public String ProductName { get; set; }
+        public String ImageURL { get; set; }
+        public int Stock { get; set; }
+        public int CostPrice { get; set; }
+        public int SellingPrice { get; set; }
+        public String Brand { get; set; }
+        public float ScreenSize { get; set; }
+        public String OS { get; set; }
+        public String Color { get; set; }
+        public int Memory { get; set; }
+        public int Storage { get; set; }
+        public int Battery { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
